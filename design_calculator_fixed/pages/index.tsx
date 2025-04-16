1e7e41bfab12a8d7b5627f2a962070d588204f79
bretthoffman99 <Brett@lindahoffmaninteriors.com>
2025-04-16 18:44:39 -0400

    Add files via upload

---
+++ design_calculator_fixed/pages/index.tsx
@@ -0,0 +1,9 @@
+import DesignCalculator from "../components/DesignCalculator";
+
+export default function Home() {
+  return (
+    <main className="p-6">
+      <DesignCalculator />
+    </main>
+  );
+}
